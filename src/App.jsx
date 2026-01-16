b2163b67f0a04240194012c111e411941d5d7a40
Denisio Rodrigues <denisio@ymail.com>
2026-01-16 13:13:01 -0300

    feat: header - opções de menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 import './App.css'
 import Logo from './components/Logo'
 
+import sacola from './images/sacola.svg' 
+import lupa from './images/perfil.svg'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const opcoesDeMenu = ['Inicio', 'Categorias', 'Minha Estante', 'Favoritos']
+  const iconesDeMenu = [lupa, sacola]
 
   return (
     <>
       <div className='app'>
         <header className='app-header'>
           <Logo/>
+          <ul className='opcoes-menu'>
+            { opcoesDeMenu.map((opcao) => (
+              <li className='opcao' key={opcao}><p>{opcao}</p></li>
+            )) }
+          </ul>
+          <ul className='opcoes-icone'>
+            { iconesDeMenu.map((icone) => (
+              <li className='icone' key={icone}><img src={icone} alt="icone menu"/></li>
+            )) }
+          </ul>
         </header>
       </div>
     </>
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -4,8 +4,8 @@ import logo from '../../images/logo.svg'
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt="logomarca do site" />
-            <p><strong>Alura</strong> Books</p>
+            <img className='logo-img' src={logo} alt="logomarca do site" />
+            <p><strong>Livraria </strong> Três Irmãos</p>
         </div>
     )
 }
