4b488af13c14013e26cd7e0fc1d0b7e25764dc01
Denisio Rodrigues <denisio@ymail.com>
2026-01-18 11:27:40 -0300

    Componente de pesquisar livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
+import Pesquisa from './components/Pesquisa'
 
-import styled from 'styled-components'
+import styled from 'styled-components' 
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -18,6 +19,7 @@ function App() {
     <>
       <AppContainer>
         <Header  />
+        <Pesquisa />
       </AppContainer>
     </>
   )
